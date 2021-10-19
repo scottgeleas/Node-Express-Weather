1957730551864a9fd3a663d2b1c890dd7f6a3de6
Scott Geleas <scottgeleas97@gmail.com>
2021-10-19 13:46:10 -0400

    files changes for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Paths for Express config
 const publicDirPath = path.join(__dirname, '../public');
@@ -95,6 +96,6 @@ app.get('*', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log('Server running on port 3000');
+app.listen(PORT, () => {
+	console.log(`Server running on ${PORT}`);
 });
